e8300bbfe4bc3002348f4e7e8d971d4be993eac5
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-06-05 21:50:40 +0200

    HMSET or rpush approaches to check

--- server.js
+++ server.js
@@ -1,46 +1,6 @@
-/*
-  {
-      productCode: 300938
-      suggestedPrice: 9.0
-  }
-  */
- function lambdaFunction(event, context) {
-    const priceOkTreshhold = 15; // % of accepted price
-
-    const request = require('request');
-    const input = event.data;
-      const baseUrl = 'api.cqz1m-softwarea1-d44-public.model-t.cc.commerce.ondemand.com'; // old ccv2
-      // const baseUrl = 'dev-com-17.accdemo.b2c.ydev.hybris.com:9002';
-      const getPriceFromCatalog = (code) => {
-        return new Promise((resolve, reject) => {
-          const fetchUrl = `https://${baseUrl}/rest/v2/electronics-spa/products/${code}?fields=code,price,name`;
-          console.log('==FETCHING', fetchUrl);
-          process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // ignore certificate errors
-          request(fetchUrl, function (error, response, body) { // Print the HTML for the Google homepage.
-            if(error) {
-              console.log('ERROR', error.statusCode, error.body);
-              reject(JSON.parse(error));
-            }
-            console.log('fetched from electronics-spa', JSON.parse(body));
-            resolve(JSON.parse(body));
-          });
-        })
-      }
-    return getPriceFromCatalog(parseInt(input.productCode)).then((result) => {
-        console.log('getPriceFromCatalog result', typeof result);
-        const price = result.price.value;
-        const suggestedPrice = parseInt(input.suggestedPrice);
-        const reducedPrice = price - (price / 100 * priceOkTreshhold);
-        const priceOk = suggestedPrice >= reducedPrice;
-        // console.log('POST', reducedPrice, suggestedPrice, priceOk);
-        return { suggestedPrice: suggestedPrice, priceAccepted: priceOk, productCode: result.code, name: result.name, 
-            version: "0.3"
-        };
-    });
-} 
-
-
-
+const priceSuggest = require('./priceSuggest');
+const priceSuggestEmail = require('./priceSuggestEmail');
+const getPriceSuggestions = require('./getPriceSuggestions');
 
 // BASE SETUP
 // =============================================================================
@@ -63,8 +23,20 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.post('/', function (req, res) {
-  lambdaFunction({ data: req.body}).then((lambdaRes) => {
+  priceSuggest({ data: req.body}).then((lambdaRes) => {
     res.json(lambdaRes);
+  })
+});
+
+router.post('/suggestemail', function (req, res) {
+  priceSuggestEmail({ data: req.body}).then((lambdaRes) => {
+    res.json(lambdaRes);
+  })
+});
+
+router.post('/suggestions', function (req, res) {
+  getPriceSuggestions({ data: req.body}).then((lambdaRes) => {
+    res.json({lambdaRes});
   })
 });
 
