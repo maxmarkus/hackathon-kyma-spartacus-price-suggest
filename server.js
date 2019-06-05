8c97802195200ed088fca54e1cce24d85581cc0c
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-06-05 13:54:13 +0200

    updated to new ccv2

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 function lambdaFunction(event, context) {
   const request = require('request');
   const priceOkTreshhold = 15; // %
-  const baseUrl = 'api.cqz1m-softwarea1-d44-public.model-t.cc.commerce.ondemand.com';
+  // const baseUrl = 'api.cqz1m-softwarea1-d44-public.model-t.cc.commerce.ondemand.com'; // old ccv2
+  const baseUrl = 'dev-com-17.accdemo.b2c.ydev.hybris.com:9002';
   /*
   {
       productCode: 300938
@@ -13,7 +14,13 @@ function lambdaFunction(event, context) {
   const getPriceFromCatalog = (code) => {
     return new Promise((resolve, reject) => {
       const fetchUrl= `https://${baseUrl}/rest/v2/electronics-spa/products/${code}?fields=code,price,name`;
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // ignore certificate errors
       request(fetchUrl, function (error, response, body) { // Print the HTML for the Google homepage.
+        if(error) {
+          console.log('ERROR', error);
+          reject(JSON.parse(error));
+        }
+        console.log('fetched', typeof body, body);
         console.log('fetched from electronics-spa', JSON.parse(body));
         resolve(JSON.parse(body));
       });
