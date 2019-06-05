f5279675519fa4f5bea1910af0f571fe8f147790
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2019-06-05 15:53:32 +0200

    event.data

--- server.js
+++ server.js
@@ -4,35 +4,45 @@
       suggestedPrice: 9.0
   }
   */
-function lambdaFunction(event, context) {
-  const request = require('request');
-  const priceOkTreshhold = 15; // %
-  // const baseUrl = 'api.cqz1m-softwarea1-d44-public.model-t.cc.commerce.ondemand.com'; // old ccv2
-  const baseUrl = 'dev-com-17.accdemo.b2c.ydev.hybris.com:9002';
-  const getPriceFromCatalog = (code) => {
-    return new Promise((resolve, reject) => {
-      const fetchUrl= `https://${baseUrl}/rest/v2/electronics-spa/products/${code}?fields=code,price,name`;
-      process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // ignore certificate errors
-      request(fetchUrl, function (error, response, body) { // Print the HTML for the Google homepage.
-        if(error) {
-          console.log('ERROR', error);
-          reject(JSON.parse(error));
-        }
-        console.log('fetched from electronics-spa', JSON.parse(body));
-        resolve(JSON.parse(body));
-      });
-    })
-  }
-  
-  return getPriceFromCatalog(parseInt(event.productCode)).then((result) => {
-    const price = result.price.value;
-    const suggestedPrice = parseInt(event.suggestedPrice);
-    const reducedPrice = price - (price / 100 * priceOkTreshhold);
-    const priceOk = suggestedPrice >= reducedPrice;
-    // console.log('POST', reducedPrice, suggestedPrice, priceOk);
-    return { suggestedPrice: suggestedPrice, priceAccepted: priceOk, productCode: result.code, name: result.name};
-  });
-}
+ function lambdaFunction(eventRaw, context) {
+      const priceOkTreshhold = 15; // % of accepted price
+
+
+    const request = require('request');
+    const event = eventRaw.data;
+      const baseUrl = 'api.cqz1m-softwarea1-d44-public.model-t.cc.commerce.ondemand.com'; // old ccv2
+      // const baseUrl = 'dev-com-17.accdemo.b2c.ydev.hybris.com:9002';
+      const getPriceFromCatalog = (code) => {
+        return new Promise((resolve, reject) => {
+          const fetchUrl = `https://${baseUrl}/rest/v2/electronics-spa/products/${code}?fields=code,price,name`;
+          console.log('==FETCHING', fetchUrl);
+          process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // ignore certificate errors
+          request(fetchUrl, function (error, response, body) { // Print the HTML for the Google homepage.
+            if(error) {
+              console.log('ERROR', error.statusCode, error.body);
+              reject(JSON.parse(error));
+            }
+            console.log('fetched from electronics-spa', JSON.parse(body));
+            resolve(JSON.parse(body));
+          });
+        })
+      }
+    return getPriceFromCatalog(parseInt(event.productCode)).then((result) => {
+        console.log('getPriceFromCatalog result', typeof result);
+        const price = result.price.value;
+        const suggestedPrice = parseInt(event.suggestedPrice);
+        const reducedPrice = price - (price / 100 * priceOkTreshhold);
+        const priceOk = suggestedPrice >= reducedPrice;
+        // console.log('POST', reducedPrice, suggestedPrice, priceOk);
+        return { suggestedPrice: suggestedPrice, priceAccepted: priceOk, productCode: result.code, name: result.name, 
+            version: "v0.2"
+        };
+    });
+} 
+
+
+
+
 // BASE SETUP
 // =============================================================================
 
@@ -54,7 +64,7 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.post('/', function (req, res) {
-  lambdaFunction(req.body).then((lambdaRes) => {
+  lambdaFunction({ data: req.body}).then((lambdaRes) => {
     res.json(lambdaRes);
   })
 });
